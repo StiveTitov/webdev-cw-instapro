50b6f884e40965ac964fb7da64750bc927c72b33
StiveTitov <123_dem_321@mail.ru>
2023-11-17 22:46:41 +0300

    add

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,23 +1,57 @@
 import { renderHeaderComponent } from "./header-component.js";
 import { posts, goToPage } from "../index.js";
+import { renderUploadImageComponent } from "./upload-image-component.js";
 
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
+  let imageUrl = "";
+
   const render = () => {
     // TODO: Реализовать страницу добавления поста
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
+      <h3 class="form-title">Добавить пост</h3>
+      <div class="upload-image-container">
+          <div class="upload=image">
+            <label class="file-upload-label secondary-button">
+                <input type="file" class="file-upload-input" style="display:none">
+                Выберите фото
+            </label>
+          </div>
+      </div>
+          <div>
+          <p>Опишите фотографию:</p>
+                <div class ="form">
+                    <div class="form-inputs">
+                        <textarea type="textarea" id="img-description" class="img-description" rows="4"></textarea>
+
+                        <button class="button" id="add-button">Добавить</button>
+                    </div>
+                </div>
+          </div>
     </div>
   `;
 
     appEl.innerHTML = appHtml;
 
+    const uploadImageContainer = appEl.querySelector(".upload-image-container");
+    
+
+    if (uploadImageContainer) {
+      renderUploadImageComponent({
+        element: appEl.querySelector(".upload-image-container"),
+        onImageUrlChange(newImageUrl) {
+          imageUrl = newImageUrl;
+        },
+      });
+    }
+
     document.getElementById("add-button").addEventListener("click", () => {
+      console.log(uploadImageContainer);
+        console.log(document.getElementById("img-description").value);
       onAddPostClick({
-        description: "Описание картинки",
-        imageUrl: "https://image.png",
+        description: document.getElementById("img-description").value,
+        imageUrl: imageUrl,
       });
     });
   };
