44856a4018407d6c3f1e6058b7b33ce200461ded
StiveTitov <123_dem_321@mail.ru>
2023-11-17 20:44:17 +0300

    добавил кнопки заголовка в страницу добавления поста

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,3 +1,6 @@
+import { renderHeaderComponent } from "./header-component.js";
+import { posts, goToPage } from "../index.js";
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     // TODO: Реализовать страницу добавления поста
@@ -20,4 +23,16 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   };
 
   render();
+
+  renderHeaderComponent({
+    element: document.querySelector(".header-container"),
+  });
+
+  for (let userEl of document.querySelectorAll(".post-header")) {
+    userEl.addEventListener("click", () => {
+      goToPage(USER_POSTS_PAGE, {
+        userId: userEl.dataset.userId,
+      });
+    });
+  }
 }
--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -1,7 +1,7 @@
 import { USER_POSTS_PAGE } from "../routes.js";
 import { renderHeaderComponent } from "./header-component.js";
 import { posts, goToPage } from "../index.js";
-import {getPosts} from "../api.js";
+
 
 export function renderPostsPageComponent({ appEl }) {
   // TODO: реализовать рендер постов из api
